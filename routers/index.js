814923dccd8324dccaee033b0ef4c710cd327c9a
Saurabh Dixit <86460997+Saurabhdixit93@users.noreply.github.com>
2023-08-10 06:19:12 +0000

    Update index.js

--- routers/index.js
+++ routers/index.js
@@ -2,6 +2,8 @@ const { Router } = require("express");
 const router = Router();
 const UserRoutes = require("./UserRoutes");
 const TaskRoutes = require("./TaskRoutes");
+// import jwt
+const { verifyToken } = require("../configuration/jwt");
 
 router.get("/", (req, res) => {
   return res.json({
@@ -9,5 +11,5 @@ router.get("/", (req, res) => {
   });
 });
 router.use("/user", UserRoutes);
-router.use("/task", TaskRoutes);
+router.use("/task", verifyToken,TaskRoutes);
 module.exports = router;
